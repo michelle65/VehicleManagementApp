edbd9dc6d934db601a29772c3be1bc85df2eb2c8
mirotariu <Mihaela.Rotariu2@endava.com>
2025-10-15 22:53:45 +0300

    created base class

---
+++ VehicleManagementApp/Models/Vehicle.cs
@@ -0,0 +1,13 @@
+﻿namespace VehicleManagementApp.Models
+{
+    public abstract class Vehicle
+    {
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public virtual string  StartEngine()
+        {
+            return "The vehicle engine starts.";
+        }
+    }
+}
