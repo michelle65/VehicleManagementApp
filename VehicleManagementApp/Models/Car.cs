31925da7e24d25759b38ff867a19428cac2b0d41
mirotariu <Mihaela.Rotariu2@endava.com>
2025-10-15 23:09:15 +0300

    created derived classes

---
+++ VehicleManagementApp/Models/Car.cs
@@ -0,0 +1,11 @@
+﻿namespace VehicleManagementApp.Models
+{
+    public class Car : Vehicle
+    {
+        public int NumberOfDoors { get; set; }
+        public override string StartEngine()
+        {
+            return "The car engine starts with a key.";
+        }
+    }
+}
---
+++ VehicleManagementApp/Models/Truck.cs
@@ -0,0 +1,11 @@
+﻿namespace VehicleManagementApp.Models
+{
+    public class Truck : Vehicle
+    {
+        public decimal CargoCapacity { get; set; }
+        public override string StartEngine()
+        {
+            return "The truck engine rumbles to life.";
+        }
+    }
+}
