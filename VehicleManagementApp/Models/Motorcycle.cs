3b3eadf43cfecad14285c255bb0780058102c03e
Michelle <rotariumihaela.bv@gmail.com>
2025-10-15 23:09:57 +0300

    created derived classes (#2)
    Co-authored-by: mirotariu <Mihaela.Rotariu2@endava.com>

---
+++ VehicleManagementApp/Models/Motorcycle.cs
@@ -0,0 +1,11 @@
+﻿namespace VehicleManagementApp.Models
+{
+    public class Motorcycle : Vehicle
+    {
+        public bool HasSidecar { get; set; }
+        public override string StartEngine()
+        {
+            return "The motorcycle engine starts with a button.";
+        }
+    }
+}
